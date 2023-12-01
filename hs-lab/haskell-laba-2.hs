383f8d98238f876779d9a1b9386a09f3781c9966
meteorizm <toomuchweer@gmail.com>
2023-12-01 17:02:15 +0300

    update laba2

--- hs-lab/haskell-laba-2.hs
+++ hs-lab/haskell-laba-2.hs
@@ -19,7 +19,60 @@ sortedEvenLst n
     | odd n = sortedEvenLst (n - 1)
     | otherwise = sortedEvenLst (n - 1) ++ [n]
 
+--Sorted list square for nums
+sortedSquareNums :: Int -> [Int]
+sortedSquareNums 0 = []
+sortedSquareNums n = sortedSquareNums (n - 1) ++ [n^2]
+
+--Sorted list for factorial num
+sortedFactNums :: Int -> [Int]
+sortedFactNums 0 = []
+sortedFactNums n = sortedFactNums (n - 1) ++ [factorial n]
+    where
+        factorial 0 = 1
+        factorial n = factorial (n - 1) * n
+
+--Sorted list for power of two num
+sortedPowOfTwoNums :: Int -> [Int]
+sortedPowOfTwoNums 0 = []
+sortedPowOfTwoNums n = sortedPowOfTwoNums (n - 1) ++ [2 ^ n]
+
+--Sorted list for triangular nums
+sortedTriangularNums :: Int -> [Int]
+sortedTriangularNums 0 = []
+sortedTriangularNums n = sortedTriangularNums (n - 1) ++ [triangular n]
+
+triangular :: Int -> Int
+triangular 0 = 0
+triangular n = n + triangular (n - 1)
+
+--Sorted list for pyramid nums
+sortedPyramidNums :: Int -> [Int]
+sortedPyramidNums 0 = []
+sortedPyramidNums n = sortedPyramidNums (n - 1) ++ [pyramid n]
+
+pyramid :: Int -> Int
+pyramid 0 = 0
+pyramid n = triangular n + pyramid (n - 1)
+
+--Func for avg real nums
+avg :: [Double] -> Double
+avg = uncurry (/) . go
+    where go [] = (0,0)
+          go (x:xs) = let (s,c) = go xs
+                      in (s+x,c+1)
+
+--Func for nums on index
+getn :: Int -> [a] -> a
+getn 0 (x:_) = x
+getn n (_:xs) = getn (n-1) xs
+
+--Sum of two lists
+sum2 :: Num a => [a] -> [a] -> [a]
+sum2 [] ys = ys
+sum2 xs [] = xs
+sum2 (x:xs) (y:ys) = (x + y) : sum2 xs ys
 
 main :: IO()
 main = do
-    print (sortedEvenLst 10)
+    print (sum2 [1,2] [1,2,3])
