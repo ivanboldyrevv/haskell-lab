2ceb9908a517257b584297233a2bc3e6e4c7d827
meteorizm <toomuchweer@gmail.com>
2023-12-06 15:35:07 +0300

    refactoring

--- hs-lab/haskell-laba-2.hs
+++ hs-lab/haskell-laba-2.hs
@@ -1,41 +1,219 @@
-sortedLst :: Int -> [Int]
-sortedLst 0 = []
-sortedLst n = sortedLst (n - 1) ++ [n]
----or simple method
-sortedLst' :: Int -> [Int]
-sortedLst' n = [1..n]
+{-
+1.  Определите функцию, принимающую на вход целое число n и возвращающую список, 
+    содержащий n элементов, упорядоченных по возрастанию.
+-}
+
+---------------------------------------------------------------------------------------------
+
+{-
+    1) Список натуральных чисел
+-}
+
+lst :: Int -> [Int]
+lst 0 = []
+lst n = lst (n - 1) ++ [n]
+
+---list comp
+lstComp :: Int -> [Int]
+lstComp n = [1..n]
+
+--map
+lstMap :: Int -> [Int]
+lstMap n = map (\x -> x) [1..n]
+
+--foldr
+lstFoldr :: Int -> [Int]
+lstFoldr n = foldr (\x s -> x : s) [] [1..n]
+
+
+
+{-  examples:
+
+    input : lst 5
+    output : [1,2,3,4,5]
+
+    input : lstComp 5
+    output : [1,2,3,4,5]
+
+    input : lstMap 5
+    output : [1,2,3,4,5]
+
+    input : lstFoldr 5
+    output : [1,2,3,4,5]
+
+-}
+
+---------------------------------------------------------------------------------------------
+
+{-
+    2) Список нечетных натуральных чисел.
+-}
 
 --Sorted list for odd nums
-sortedOddLst :: Int -> [Int]
-sortedOddLst 0 = []
-sortedOddLst n
-    | even n = sortedOddLst (n - 1)
-    | otherwise = sortedOddLst (n - 1) ++ [n]
+oddLst :: Int -> [Int]
+oddLst 0 = []
+oddLst n
+    | even n = oddLst (n - 1)
+    | otherwise = oddLst (n - 1) ++ [n]
+
+--or list comp
+oddLstComp :: Int -> [Int]
+oddLstComp n = [x | x <- [1..n], odd x]
+
+--foldr
+oddLstFoldr :: Int -> [Int]
+oddLstFoldr n = foldr (\x s -> if odd x then x : s else s) [] [1..n]
+
+--filter
+
+oddLstFilter :: Int -> [Int]
+oddLstFilter n = filter odd [1..n]
+
+{-  
+    examples: 
+    
+    oddLst 5
+    Output : [1,3,5,7,9]
+
+    oddLstComp 5
+    Output : [1,3,5,7,9]
+
+    oddLstFoldr 5
+    Output : [1,3,5,7,9]
+
+    oddLstFilter 5
+    Output : [1,3,5,7,9]
+-}
+
+---------------------------------------------------------------------------------------------
+
+{-
+    3) Список четных натуральных чисел.
+-}
 
 --Sorted list for even nums
-sortedEvenLst :: Int -> [Int]
-sortedEvenLst 0 = []
-sortedEvenLst n
-    | odd n = sortedEvenLst (n - 1)
-    | otherwise = sortedEvenLst (n - 1) ++ [n]
+evens :: Int -> [Int]
+evens 0 = []
+evens n
+    | odd n = evens (n - 1)
+    | otherwise = evens (n - 1) ++ [n]
+
+--list comp
+evenLstComp :: Int -> [Int]
+evenLstComp n = [x | x <- [1..n], even x]
+
+--foldr 
+evenFoldr :: Int -> [Int]
+evenFoldr n = foldr (\x s -> if even x then x : s else s) [] [1..n]
+
+--filter
+evenFilter :: Int -> [Int]
+evenFilter n = filter even [1..n]
+
+{-
+    examples:
+
+    input: evens 10
+    output: [2,4,6,8,10]
+
+    input: evenLstComp 10
+    output: [2,4,6,8,10]
+
+    input: evenFoldr 10
+    output: [2,4,6,8,10]
+
+    input: evenFilter 10
+    output: [2,4,6,8,10]
+-}
+
+---------------------------------------------------------------------------------------------
+
+{-
+    4) Список квадратов натуральных чисел.
+-}
 
 --Sorted list square for nums
-sortedSquareNums :: Int -> [Int]
-sortedSquareNums 0 = []
-sortedSquareNums n = sortedSquareNums (n - 1) ++ [n^2]
+squareNums :: Int -> [Int]
+squareNums 0 = []
+squareNums n = squareNums (n - 1) ++ [n^2]
+
+--lst comp
+squareNumsLstComp :: Int -> [Int]
+squareNumsLstComp n = [x^2 | x <- [1..n]]
+
+--foldr 
+squareNumsFoldr :: Int -> [Int]
+squareNumsFoldr n = foldr (\x s -> x^2 : s) [] [1..n]
+
+--map
+squareNumsMap :: Int -> [Int]
+squareNumsMap n = map (^2) [1..n]
+
+{-
+    examples:
+    
+    input: squareNums 10
+    output: [1,4,9,16,25,36,49,64,81,100]
+
+    input: squareNumsLstComp 10
+    output: [1,4,9,16,25,36,49,64,81,100]
+
+    input: squareNumsFoldr 10
+    output: [1,4,9,16,25,36,49,64,81,100]
+
+    input: squareNumsMap 10
+    output: [1,4,9,16,25,36,49,64,81,100]
+-}
+
+---------------------------------------------------------------------------------------------
+
+{-
+    5)  Список факториалов.
+-}
 
 --Sorted list for factorial num
-sortedFactNums :: Int -> [Int]
-sortedFactNums 0 = []
-sortedFactNums n = sortedFactNums (n - 1) ++ [factorial n]
+factNums :: Int -> [Int]
+factNums 0 = []
+factNums n = factNums (n - 1) ++ [factorial n]
     where
         factorial 0 = 1
         factorial n = factorial (n - 1) * n
+
+--list comp
+factLstComp :: Int -> [Int]
+factLstComp n = [factorial x | x <- [1..n]]
+    where factorial n = foldr (*) 1 [1..n]
+
+--map
+factMap :: Int -> [Int]
+factMap n = map factorial [1..n]
+    where
+        factorial x = product [1..n]
+
+--foldr 
+factFoldr :: Int -> [Int]
+factFoldr n = foldr (\x s -> factorial x : s) [] [1..n]
+    where
+        factorial n = product [1..n]
+
+{-
+    examples:
+-}
+
+---------------------------------------------------------------------------------------------
+
+{-
+    6) Список степеней двойки.
+-}
 
 --Sorted list for power of two num
 sortedPowOfTwoNums :: Int -> [Int]
 sortedPowOfTwoNums 0 = []
 sortedPowOfTwoNums n = sortedPowOfTwoNums (n - 1) ++ [2 ^ n]
+
+{-
+    7) Список треугольных чисел.
+-}
 
 --Sorted list for triangular nums
 sortedTriangularNums :: Int -> [Int]
@@ -45,6 +223,10 @@ sortedTriangularNums n = sortedTriangularNums (n - 1) ++ [triangular n]
 triangular :: Int -> Int
 triangular 0 = 0
 triangular n = n + triangular (n - 1)
+
+{-
+    8) Список пирамидальных чисел.
+-}
 
 --Sorted list for pyramid nums
 sortedPyramidNums :: Int -> [Int]
@@ -91,4 +273,4 @@ makePositive = map (\x -> abs x)
 
 main :: IO()
 main = do
-    print (makePositive  [-1, 0, 5, -10, -20])
+    print (lstFoldr 10)
